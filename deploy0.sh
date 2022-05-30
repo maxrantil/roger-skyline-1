ce16af5cbd2548c06603978e44ef79c3cc3b7c3c
Max <rantil@pm.me>
2022-05-30 12:28:30 +0300

    artix script partition v1

--- deploy0.sh
+++ deploy0.sh
@@ -1,41 +1,67 @@
 #!/bin/bash
 
+# Script for auto-installing Artix linux from scratch
 # 4 partitions with swap
 
-sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk ${TGTDEV}
-  n # new partition
-  p # primary partition
-    # partition number 1
-    # default - start at beginning of disk 
-  +1G # 1Gb boot parttion
-  n # new partition
-  p # primary partition
-    # partion number 2
-    # default, start immediately after preceding partition
-  +2.3G # 2.3Gb swap partition
-  n # new partition
-  p # primary partition
-    # partion number 3
-    # default, start immediately after preceding partition
-  +4.2G # 4.2Gb root parttion
-  n # new partition
-  p # primary partition
-    # partion number 4
-    # default, start immediately after preceding partition
-    # rest of disk space for last partition
-  w # write the partition table
-  q # and we're done
-EOF
+dialog --no-cancel --inputbox "Enter a name for your computer." 10 60 2> comp
 
-sudo mkfs.ext4 -L BOOT /dev/sda1
-sudo mkfs.ext4 -L ROOT /dev/sda3
-sudo mkfs.ext4 -L HOME /dev/sda4
-sudo mkswap -L SWAP /dev/sda2
-sudo swapon /dev/sda2/SWAP
-sudo mount /dev/sda3/ROOT /mnt
-sudo mkdir /mnt/home
-sudo mkdir /mnt/boot
-sudo mount /dev/sda1/BOOT /mnt/boot
-sudo mount /dev/sda4/HOME /mnt/home
+dialog --defaultno --title "Time Zone select" --yesno "Do you want use zone(Europe/Helsinki)?.\n\nPress no for select your own time zone"  10 60 && echo "Europe/Helsinki" > tz.tmp || tzselect > tz.tmp
+
+dialog --no-cancel --inputbox "Enter partitionsize in gb, separated by space (swap & root)." 10 60 2>psize
+
+IFS=' ' read -ra SIZE <<< $(cat psize)
+
+re='^[0-9]+$'
+if ! [ ${#SIZE[@]} -eq 2 ] || ! [[ ${SIZE[0]} =~ $re ]] || ! [[ ${SIZE[1]} =~ $re ]] ; then
+    SIZE=(12 25);
+fi
+
+timedatectl set-ntp true
+
+cat <<EOF | fdisk /dev/sda
+o
+n
+p
+
+
++1G
+n
+p
+
+
++${SIZE[0]}G
+n
+p
+
+
++${SIZE[1]}G
+n
+p
+
+
+w
+EOF
+partprobe
+
+yes | mkfs.ext4 -L BOOT /dev/sda1
+yes | mkfs.ext4 -L ROOT /dev/sda3
+yes | mkfs.ext4 -L HOME /dev/sda4
+mkswap -L SWAP /dev/sda2/SWAP
+swapon /dev/sda2/SWAP
+mount /dev/sda3/ROOT /mnt
+mkdir -p /mnt/boot
+mount /dev/sda1 /mnt/boot
+mkdir -p /mnt/home
+mount /dev/sda4 /mnt/home
+# sudo mkfs.ext4 -L BOOT /dev/sda1
+# sudo mkfs.ext4 -L ROOT /dev/sda3
+# sudo mkfs.ext4 -L HOME /dev/sda4
+# sudo mkswap -L SWAP /dev/sda2
+# sudo swapon /dev/sda2/SWAP
+# sudo mount /dev/sda3/ROOT /mnt
+# sudo mkdir /mnt/home
+# sudo mkdir /mnt/boot
+# sudo mount /dev/sda1/BOOT /mnt/boot
+# sudo mount /dev/sda4/HOME /mnt/home
 
 
