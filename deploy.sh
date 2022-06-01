43cd23603ca7860ecda9e8884fa7cb9ad06ce222
Max <rantil@pm.me>
2022-06-01 20:16:17 +0300

    keyrings update try2

--- deploy.sh
+++ deploy.sh
@@ -5,30 +5,14 @@
 
 ## FUNCTIONS
 ###
-refreshkeys() { \
-	case "$(readlink -f /sbin/init)" in
-		*systemd* )
-			dialog --infobox "Refreshing Arch Keyring..." 4 40
-			pacman --noconfirm -S archlinux-keyring >/dev/null 2>&1
-			;;
-		*)
-			dialog --infobox "Enabling Arch Repositories..." 4 40
-			pacman --noconfirm --needed -S artix-keyring artix-archlinux-support >/dev/null 2>&1
-			for repo in extra community; do
-				grep -q "^\[$repo\]" /etc/pacman.conf ||
-					echo "[$repo]
-Include = /etc/pacman.d/mirrorlist-arch" >> /etc/pacman.conf
-			done
-			pacman -Sy >/dev/null 2>&1
-			pacman-key --populate archlinux >/dev/null 2>&1
-			;;
-	esac ;}
+
 
 ## Script Main starts here
 ###
 
-dialog --no-cancel --inputbox "Enter a name for your computer(e.g. 'desktop')." 10 60 2> comp
+pacman --noconfirm --needed -Sy dialog || error "Are you sure you're running this as the root user, are on an Arch-based distribution and have an internet connection?"
 
+dialog --no-cancel --inputbox "Enter a name for your computer(e.g. 'desktop')." 10 60 2> comp
 dialog --defaultno --title "Time Zone select" --yesno "Do you want use zone(Europe/Helsinki)?.\n\nPress no for select your own time zone"  10 60 && echo "Europe/Helsinki" > tz.tmp || tzselect > tz.tmp
 dialog --no-cancel --inputbox "Enter partitionsize in gb, separated by space (swap & root)." 10 60 2>psize
 
@@ -81,7 +65,14 @@ grep -q "ILoveCandy" /etc/pacman.conf || sed -i "/#VerbosePkgLists/a ILoveCandy"
 sed -Ei "s/^#(ParallelDownloads).*/\1 = 5/;/^#Color$/s/#//" /etc/pacman.conf
 
 # Add mirrors
-refreshkeys || error "Error automatically refreshing Arch keyring. Consider doing so manually."
+pacman --noconfirm --needed -S artix-keyring artix-archlinux-support >/dev/null 2>&1
+			for repo in extra community; do
+				grep -q "^\[$repo\]" /etc/pacman.conf ||
+					echo "[$repo]
+Include = /etc/pacman.d/mirrorlist-arch" >> /etc/pacman.conf
+			done
+			pacman -Sy >/dev/null 2>&1
+			pacman-key --populate archlinux >/dev/null 2>&1
 #echo "Server = https://ftp.ludd.ltu.se/mirrors/artix/$repo/os/$arch" > mirrors
 #echo "Server = https://mirrors.dotsrc.org/artix-linux/repos/$repo/os/$arch" >> mirrors
 #echo "Server = https://mirror.one.com/artix/$repo/os/$arch" >> mirrors
