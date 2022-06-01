14954e174b602c33eb742ba28c8f83a4464ab472
Max <rantil@pm.me>
2022-06-01 21:06:45 +0300

    keyrings update try6

--- chroot.sh
+++ chroot.sh
@@ -8,7 +8,7 @@ rm tzfinal.tmp
 
 hwclock --systohc
 
-echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 echo "en_US ISO-8859-1" >> /etc/locale.gen
 locale-gen
