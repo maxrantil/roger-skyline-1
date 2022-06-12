8bb382cf514ca93900759aa5ff5845c71f81e225
Max <rantil@pm.me>
2022-06-12 17:39:16 +0300

    sunday coding sessing with mqx5

--- setup.sh
+++ setup.sh
@@ -254,7 +254,9 @@ bash gen_certificates.sh
 #cp server.key server.key.org
 #openssl rsa -in server.key.org -out server.key
 #openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt
+echo "before"
 sed -i '/#Include conf/extra/httpd-ssl.conf/s/^#//g' /etc/httpd/conf/httpd.conf
+echo "after"
 sv restart apache
 
 ## List all services
