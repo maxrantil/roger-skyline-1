a5f1aaf37a444aca005a2a54e44e56054c9ae0f6
Max <rantil@pm.me>
2022-06-15 19:27:59 +0300

    wednesday SSL  session5

--- setup.sh
+++ setup.sh
@@ -39,6 +39,7 @@ getip() { \
 		ethernet=$(ip r | awk '/'$gateway'/ {print $9}')
 		broadcast=$(ip a | awk '/'$ethernet'/ {print $4}')
 		eth_mask=$(ip a | awk '/'$ethernet'/ {print $2}')
+		hostname=$(cat /etc/hostname)
 		}
 
 ## Script Main starts here
@@ -229,11 +230,13 @@ bash gen_certificates.sh
 #sed -i 's/Listen 80/Listen "'${ethernet}':80"/g' /etc/httpd/conf/httpd.conf
 sed -i '/#LoadModule ssl_module modules\/mod_ssl.so/s/^#//g' /etc/httpd/conf/httpd.conf
 sed -i '/#LoadModule socache_shmcb_module modules\/mod_socache_shmcb.so/s/^#//g' /etc/httpd/conf/httpd.conf
-#sed -i 's/ServerAdmin you@example.com/ServerAdmin "'${name}@${hostname}'"/g' /etc/httpd/conf/httpd.conf
+sed -i 's/ServerAdmin you@example.com/ServerAdmin "'${name}@${hostname}'"/g' /etc/httpd/conf/httpd.conf
 sed -i 's/#ServerName www.example.com:80/ServerName "localhost:80"/g' /etc/httpd/conf/httpd.conf
 #sed -i 's/DocumentRoot "\/srv\/httpd"/DocumentRoot "/srv/'${ethernet}'"/g' /etc/httpd/conf/httpd.conf
 #sed -i 's/<Directory "\/srv\/httpd"/<Directory "/srv/'${ethernet}'"/g' /etc/httpd/conf/httpd.conf
 sed -i '/#Include conf\/extra\/httpd-ssl.conf/s/^#//g' /etc/httpd/conf/httpd.conf
+sed -i 's/ServerName www.example.com:443/ServerName "localhost:443"/g' /etc/httpd/conf/extra/httpd-ssl.conf
+sed -i 's/ServerAdmin you@example.com:443/ServerName "'${name}@${hostname}':443"/g' /etc/httpd/conf/extra/httpd-ssl.conf
 
 ## Website
 echo Redirect \"/\" \"https://"${ethernet}"\" >> /etc/httpd/conf/httpd.conf
