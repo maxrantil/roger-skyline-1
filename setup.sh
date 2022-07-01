77a07be6123f5e2fe5c4b251295ad24ae569e44b
Max <rantil@pm.me>
2022-07-01 21:14:35 +0300

    final touches

--- setup.sh
+++ setup.sh
@@ -343,7 +343,8 @@ mv update_packages /etc/cron.weekly
 #echo new cron into cron file
 echo "# Update source to packages
 @reboot		/etc/cron.weekly/update_packages	>/dev/null 2>&1
-@midnight	~/scripts/monitor_cronfile.sh		>/dev/null 2>&1" >> mycron
+@midnight	~/scripts/monitor_cronfile.sh		>/dev/null 2>&1
+@reboot		~/scripts/reload_iptables.sh		>/dev/null 2>&1" >> mycron
 #install new cron file
 crontab mycron
 rm mycron
@@ -381,6 +382,21 @@ fi
 EOF
 chmod 755 monitor_cronfile.sh
 mv monitor_cronfile.sh scripts
+
+cat > reload_iptables.sh <<'EOF'
+#!/bin/sh
+
+ipset create port_scanners hash:ip family inet hashsize 32768 maxelem 65536 timeout 600
+ipset create scanned_ports hash:ip,port family inet hashsize 32768 maxelem 65536 timeout 60
+iptables -A INPUT -m state --state INVALID -j DROP
+iptables -A INPUT -m state --state NEW -m set ! --match-set scanned_ports src,dst -m hashlimit --hashlimit-above 1/hour --hashlimit-burst 5 --hashlimit-mode srcip --hashlimit-name portscan --hashlimit-htable-expire 10000 -j SET --add-set port_scanners src --exist
+iptables -A INPUT -m state --state NEW -m set --match-set port_scanners src -j DROP
+iptables -A INPUT -m state --state NEW -j SET --add-set scanned_ports src,dst
+iptables -A INPUT -p tcp -m tcp -m multiport ! --dports 80,443 -j DROP
+EOF
+echo "iptables -A INPUT -p tcp -m tcp -m multiport ! --dports ${port} -j DROP" >> reload_iptables.sh
+chmod 755 reload_iptables.sh
+mv reload_iptables.sh scripts
 
 #pacman -S --noconfirm postfix postfix-runit
 installpkg postfix
