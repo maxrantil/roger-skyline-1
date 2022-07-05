5f2d2adffc272013247517eced66beaf93e609dc
Max <rantil@pm.me>
2022-07-05 17:42:08 +0300

    final

--- setup.sh
+++ setup.sh
@@ -33,10 +33,10 @@ securessh() { \
 		}
 
 getip() { \
-		device=$(nmcli con show | awk '/DEVICE/ {getline ; print $NF}')
-		con_name=$(nmcli con show | awk '/DEVICE/ {getline ; print $1" "$2" "$3}')
+		device=$(nmcli con show | awk '/DEVICE/ {getline ; getline ; print $NF}')
+		con_name=$(nmcli con show | awk '/DEVICE/ {getline ; getline ; print $1" "$2" "$3}')
 		gateway=$(ip r | awk '/default/ {print $3}')
-		ethernet=$(ip r | awk '/'$gateway'/ {print $9}')
+		ethernet=$(ip r | awk '/'$gateway'/ {getline ; print $9}')
 		broadcast=$(ip a | awk '/'$ethernet'/ {print $4}')
 		eth_mask=$(ip a | awk '/'$ethernet'/ {print $2}')
 		}
