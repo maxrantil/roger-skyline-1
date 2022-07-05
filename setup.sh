de712c7d8d60d456b5e71707670915cf98ab58f4
Max <rantil@pm.me>
2022-07-05 19:41:56 +0300

    final

--- setup.sh
+++ setup.sh
@@ -133,14 +133,18 @@ bantime = 6000" >> /etc/fail2ban/jail.local
 
 sv restart fail2ban
 
+
+#outgoing traffic allowed
+iptables -I OUTPUT -o eth0 -j ACCEPT
+iptables -I INPUT -i eth0 -m state --state ESTABLISHED,RELATED -j ACCEPT
+# portscanning attack protection
 ipset create port_scanners hash:ip family inet hashsize 32768 maxelem 65536 timeout 600
 ipset create scanned_ports hash:ip,port family inet hashsize 32768 maxelem 65536 timeout 60
-iptables -I OUTPUT -o eth1 -j ACCEPT
-iptables -I INPUT -i eth1 -m state --state ESTABLISHED,RELATED -j ACCEPT
 iptables -A INPUT -m state --state INVALID -j DROP
 iptables -A INPUT -m state --state NEW -m set ! --match-set scanned_ports src,dst -m hashlimit --hashlimit-above 1/hour --hashlimit-burst 5 --hashlimit-mode srcip --hashlimit-name portscan --hashlimit-htable-expire 10000 -j SET --add-set port_scanners src --exist
 iptables -A INPUT -m state --state NEW -m set --match-set port_scanners src -j DROP
 iptables -A INPUT -m state --state NEW -j SET --add-set scanned_ports src,dst
+# firewall
 iptables -A INPUT -p tcp -m tcp -m multiport ! --dports 80,443,${port} -j DROP
 
 ## Save the rules
